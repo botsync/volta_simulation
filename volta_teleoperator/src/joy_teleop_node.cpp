298839b6fcffe7a01e60ec7a170351ed440a5d2d
Mahendra-botsync <mahendra.l@botsync.co>
2020-05-13 10:53:41 +0530

    Added copyright for cpp files

--- volta_teleoperator/src/joy_teleop_node.cpp
+++ volta_teleoperator/src/joy_teleop_node.cpp
@@ -5,6 +5,7 @@ All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are met:
+
     * Redistributions of source code must retain the above copyright
       notice, this list of conditions and the following disclaimer.
     * Redistributions in binary form must reproduce the above copyright
@@ -24,6 +25,9 @@ LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
 ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+\author Botsync Pte. Ltd.
+\Allows to use a joystick control along with keyboard control
 
 */
 
